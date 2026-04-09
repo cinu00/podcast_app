a16a2502f83fb2bcf12f5bd42ef118137d39d20e
cinu00 <marcinkubiak525@gmail.com>
2026-04-09 21:42:31 +0200

    sync

--- app.py
+++ app.py
@@ -1,37 +1,31 @@
 import streamlit as st
-from pydub import AudioSegment
 import subprocess
 
-
-import shutil
-
-print("FFMPEG:", shutil.which("ffmpeg"))
-print("FFPROBE:", shutil.which("ffprobe"))
-
-from pydub import AudioSegment
+FFMPEG_PATH = "/layers/digitalocean_apt/apt/usr/bin/ffmpeg"
 
 def extract_audio(uploaded_file):
-    # upewnij się, że czytamy od początku
     uploaded_file.seek(0)
 
-    # zapis pliku na dysk
     with open("temp.mp4", "wb") as f:
         f.write(uploaded_file.read())
 
-    # 🔍 DEBUG - sprawdzamy co widzi ffprobe
-    result = subprocess.run(
-        ["ffprobe", "temp.mp4"],
-        capture_output=True,
-        text=True
-    )
-    print("FFPROBE OUTPUT:", result.stdout)
-    print("FFPROBE ERR:", result.stderr)
+    command = [
+        FFMPEG_PATH,
+        "-i", "temp.mp4",
+        "-vn",
+        "-acodec", "pcm_s16le",
+        "-ar", "44100",
+        "-ac", "2",
+        "audio.wav"
+    ]
 
-    # konwersja audio
-    audio = AudioSegment.from_file("temp.mp4")
-    audio.export("audio.wav", format="wav")
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    print("FFMPEG STDERR:", result.stderr)
 
     return "audio.wav"
+
+
 st.title("Aplikacja do przetwarzania multimediów")
 
 uploaded_file = st.file_uploader("Prześlij plik audio lub wideo", type=["mp3", "mp4", "wav"])
@@ -39,15 +33,13 @@ uploaded_file = st.file_uploader("Prześlij plik audio lub wideo", type=["mp3", 
 if uploaded_file is not None:
     file_type = uploaded_file.type.split('/')[0]
 
-    # Wyświetlanie audio
     if file_type == 'audio':
         st.audio(uploaded_file, format=uploaded_file.type)
 
-    # Wyświetlanie wideo z możliwością ekstrakcji audio
     elif file_type == 'video':
         st.video(uploaded_file)
 
         if st.button("Wyodrębnij audio z wideo"):
             audio_file_path = extract_audio(uploaded_file)
             st.success(f"Audio wyodrębnione pomyślnie! {audio_file_path}")
-            st.audio(audio_file_path, format="audio/mp3")
+            st.audio(audio_file_path, format="audio/wav")
